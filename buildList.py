5d10c925f9fcf3e3463060e7657b64c8de72bda8
Jonas Sharron <jfsharron@msn.com>
2022-10-15 07:06:40 -0700

    Delete buildList.py
    purging unnecessary files

--- buildList.py
+++
@@ -1,9 +0,0 @@
-from isbnlib import meta
-from isbnlib.registry import bibformatters
-
-SERVICE = "openl"
-
-# now you can use the service
-isbn = "9780764548338"
-bibtex = bibformatters["bibtex"]
-print(bibtex(meta(isbn, SERVICE)))
